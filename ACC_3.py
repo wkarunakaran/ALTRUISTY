00cf5bc9605728c7ec7c503e0ec5d03e70c24f14
wkarunakaran <146978759+wkarunakaran@users.noreply.github.com>
2024-09-08 18:33:59 +0530

    Add files via upload

---
+++ ACC_3.py
@@ -0,0 +1,14 @@
+def getMaxToys(prices, money):
+    max_toys = 0
+    current_sum = 0
+    left = 0
+    for right in range(len(prices)):
+        current_sum += prices[right]
+        while current_sum > money:
+            current_sum -= prices[left]
+            left += 1
+        max_toys = max(max_toys, right - left + 1)
+    return max_toys
+prices = [1, 4, 5, 3, 2, 1, 6]
+money = 6
+print(getMaxToys(prices, money))  
